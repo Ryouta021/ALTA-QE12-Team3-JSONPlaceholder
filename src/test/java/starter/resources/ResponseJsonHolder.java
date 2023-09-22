ad187bc286ea3ae092951b24a2ba523f364677c4
ryouta222 <alfiansepty01@gmail.com>
2023-09-22 17:04:28 +0700

    feature users update

--- src/test/java/starter/resources/ResponseJsonHolder.java
+++ src/test/java/starter/resources/ResponseJsonHolder.java
@@ -1,11 +1,13 @@
-package starter.reqres;
+package starter.resources;
 
-public class ReqresResponses {
+public class ResponseJsonHolder {
 
     public static String PAGE = "page";
     public static String NAME = "name";
     public static String JOB = "job";
     public static String DATA_ID_ARRY = "data[0].id";
     public static String DATA_ID = "data.id";
+    public static String ID_POST_USER = "id";
+
 
 }
