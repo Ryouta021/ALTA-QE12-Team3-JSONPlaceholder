90e30efdcc0460cb4458c505315d891fd4631c6b
ryouta222 <alfiansepty01@gmail.com>
2023-09-22 17:09:34 +0700

    Update ResponseJsonHolder.java

--- src/test/java/starter/resources/ResponseJsonHolder.java
+++ src/test/java/starter/resources/ResponseJsonHolder.java
@@ -7,6 +7,7 @@ public class ResponseJsonHolder {
     public static String JOB = "job";
     public static String DATA_ID_ARRY = "data[0].id";
     public static String DATA_ID = "data.id";
+    public static String ID_POST = "id";
     public static String ID_POST_USER = "id";
 
 
